c158df70cf062d9a60428beb06fa316ebdb1cec5
JohnBarron <JohnBarron@users.noreply.github.com>
2015-09-25 12:01:37 -0400

    Following a tutorial, made a button and a text input field.

--- app/src/main/java/csc535/barcodescanninginventorymanagement/MainActivity.java
+++ app/src/main/java/csc535/barcodescanninginventorymanagement/MainActivity.java
@@ -4,13 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView mainTextView;
+    Button mainButton;
+    EditText mainEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Access the TextView defined in layout XML and set its text
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java!");
+
+        // Access the Button defined in layout XML and listen for it here
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+
+        // Access the EditText defined in layout XML
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
     }
 
     @Override
@@ -21,17 +40,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onClick(View v) {
+// Take what was typed into the EditText
+        // and use in TextView
+        mainTextView.setText(mainEditText.getText().toString()
+                + " is learning Android development!");
     }
 }
